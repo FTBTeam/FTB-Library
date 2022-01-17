c13b75a9fe41414461053d7d1d1e7fc7425fae5d
Max <maxh2709@gmail.com>
2022-01-17 21:11:59 +0100

    Mark REI plugin as client only

--- forge/src/main/java/dev/ftb/mods/ftblibrary/integration/forge/REIForgePluginStub.java
+++ forge/src/main/java/dev/ftb/mods/ftblibrary/integration/forge/REIForgePluginStub.java
@@ -2,7 +2,8 @@ package dev.ftb.mods.ftblibrary.integration.forge;
 
 import dev.ftb.mods.ftblibrary.integration.REIIntegration;
 import me.shedaniel.rei.forge.REIPlugin;
+import net.minecraftforge.api.distmarker.Dist;
 
-@REIPlugin
+@REIPlugin(Dist.CLIENT)
 public class REIForgePluginStub extends REIIntegration {
 }
