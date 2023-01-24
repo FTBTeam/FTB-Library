db9b5825a8043d7b918d3c8dbf9bc068fe445f2f
Des Herriott <des.herriott@gmail.com>
2023-01-24 08:45:26 +0000

    fix: don't try to relativize paths on SNBT load failure
    No guarantee that relativize() will work, and that just masks the
    actual SNBT load error, making debugging difficult.
    
    https://github.com/FTBTeam/FTB-Mods-Issues/issues/645

--- common/src/main/java/dev/ftb/mods/ftblibrary/snbt/SNBT.java
+++ common/src/main/java/dev/ftb/mods/ftblibrary/snbt/SNBT.java
@@ -1,6 +1,5 @@
 package dev.ftb.mods.ftblibrary.snbt;
 
-import dev.architectury.platform.Platform;
 import dev.ftb.mods.ftblibrary.FTBLibrary;
 import net.minecraft.nbt.*;
 import org.jetbrains.annotations.Nullable;
@@ -27,10 +26,10 @@ public class SNBT {
 		try {
 			return readLines(Files.readAllLines(path, StandardCharsets.UTF_8));
 		} catch (SNBTSyntaxException ex) {
-			FTBLibrary.LOGGER.error("Failed to read " + Platform.getGameFolder().relativize(path) + ": " + ex.getMessage());
+			FTBLibrary.LOGGER.error("Failed to read " + path + ": " + ex.getMessage());
 			return null;
 		} catch (Exception ex) {
-			FTBLibrary.LOGGER.error("Failed to read " + Platform.getGameFolder().relativize(path) + ": " + ex);
+			FTBLibrary.LOGGER.error("Failed to read " + path + ": " + ex);
 			ex.printStackTrace();
 			return null;
 		}
