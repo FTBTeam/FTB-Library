e8c8fd3558d18b03f4eb1074fb6ee45655cf5d5b
Des Herriott <des.herriott@gmail.com>
2023-02-13 09:38:32 +0000

    chore: convert existing "tests" to proper JUnit tests
    Scope for more tests to be written, but this gives some basic
    test coverage for SNBT handling and SNBT configs
    
    Include a gradle test run as part of CI process

--- common/src/test/java/dev/ftb/mods/ftblibrary/snbt/SNBTTest.java
+++ common/src/test/java/dev/ftb/mods/ftblibrary/snbt/SNBTTest.java
@@ -2,17 +2,75 @@ package dev.ftb.mods.ftblibrary.snbt;
 
 import io.netty.buffer.Unpooled;
 import net.minecraft.nbt.CompoundTag;
+import net.minecraft.nbt.DoubleTag;
 import net.minecraft.nbt.ListTag;
+import net.minecraft.nbt.Tag;
 import net.minecraft.network.FriendlyByteBuf;
+import org.junit.jupiter.api.Test;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.nio.charset.StandardCharsets;
-import java.util.Objects;
 import java.util.stream.Collectors;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 public class SNBTTest {
-	public static void main(String[] args) {
+	@Test
+	void testWriteAndReadbackString() {
+		var tag = makeTestCompound();
+
+		var lines = SNBT.writeLines(tag);
+		CompoundTag reverseTag = SNBT.readLines(lines);
+
+		assertEquals(tag, reverseTag, "Reverse tag test");
+	}
+
+	@Test
+	void testWriteAndReadbackNet() {
+		var tag = makeTestCompound();
+
+		var byteBuf = new FriendlyByteBuf(Unpooled.buffer());
+		SNBTNet.write(byteBuf, tag);
+		byteBuf.setIndex(0, byteBuf.capacity());
+		var netTag = SNBTNet.readCompound(byteBuf);
+
+		assertEquals(tag, netTag, "Network IO test");
+	}
+
+	@Test
+	void testReadResourceStream() throws IOException {
+		try (var stream = SNBTTest.class.getResourceAsStream("/snbt_test.snbt")) {
+			assertNotNull(stream);
+
+			CompoundTag newParserTest = SNBT.readLines(new BufferedReader(new InputStreamReader(stream, StandardCharsets.UTF_8)).lines().collect(Collectors.toList()));
+
+			var lines2 = SNBT.writeLines(newParserTest);
+			CompoundTag newParserTest2 = SNBT.readLines(lines2);
+
+			assertEquals(newParserTest, newParserTest2, "New parser reverse tag test");
+
+			assertEquals(newParserTest.getString("test_string"), "value", "getString");
+			assertTrue(newParserTest.getBoolean("testBool"), "getBoolean");
+			assertEquals(newParserTest.getInt("testInt"), 1234, "getInt");
+			assertEquals(newParserTest.getShort("testShort"), 49, "getShort");
+			assertEquals(newParserTest.getLong("testLong"), 304993938434993L, "getShort");
+			assertEquals(newParserTest.getIntArray("intArray")[1], 49, "getIntArray");
+			assertEquals(newParserTest.getByteArray("byteArray")[1], 49, "getByteArray");
+			assertEquals(newParserTest.getLongArray("longArray")[1], -34348L, "getLongArray");
+			assertTrue(Double.isInfinite(newParserTest.getDouble("testDouble")), "getDouble (infinity)");
+			assertEquals(newParserTest.getList("testList", Tag.TAG_STRING).getString(2), "c $##@! 'string' 3", "getList (string)");
+
+			assertFalse(newParserTest.contains("missingField"), "check for missing field");
+
+			CompoundTag subTag = newParserTest.getCompound("testCompound");
+			assertNotNull(subTag, "testCompound presence");
+			assertEquals(subTag.getInt("s1"), 5, "testCompound integer");
+		}
+	}
+
+	private SNBTCompoundTag makeTestCompound() {
 		var tag = new SNBTCompoundTag();
 		tag.comment("hi", "Hello (Boolean test)");
 		tag.putBoolean("hi", false);
@@ -35,30 +93,18 @@ public class SNBTTest {
 		tag.putInt("test_int", 30);
 		tag.putNull("test_null");
 
-		var lines = SNBT.writeLines(tag);
-		CompoundTag reverseTag = SNBT.readLines(lines);
+		ListTag list = new ListTag();
+		list.add(DoubleTag.valueOf(0.0001d));
+		list.add(DoubleTag.valueOf(5.3d));
+		list.add(DoubleTag.valueOf(-4.56e-4d));
+		tag.put("double_list", list);
 
-		System.out.println("Reverse tag test: " + (reverseTag != null ? "success" : "fail"));
+		SNBTCompoundTag subTag = new SNBTCompoundTag();
+		subTag.putInt("int_field", 1);
+		subTag.putString("string_field", "hey");
+		subTag.putBoolean("bool_field", true);
+		tag.put("compound", subTag);
 
-		try (var stream = SNBTTest.class.getResourceAsStream("/snbt_test.snbt")) {
-			CompoundTag newParserTest = SNBT.readLines(new BufferedReader(new InputStreamReader(stream, StandardCharsets.UTF_8)).lines().collect(Collectors.toList()));
-
-			var lines2 = SNBT.writeLines(newParserTest);
-			CompoundTag newParserTest2 = SNBT.readLines(lines2);
-
-			System.out.println("New parser reverse tag test: " + (Objects.equals(newParserTest, newParserTest2) ? "success" : "fail"));
-		} catch (SNBTSyntaxException ex) {
-			System.out.println("New parser test: failed: " + ex.getMessage());
-		} catch (Exception ex) {
-			ex.printStackTrace();
-			System.out.println("New parser test: failed: " + ex);
-		}
-
-		var byteBuf = new FriendlyByteBuf(Unpooled.buffer());
-		SNBTNet.write(byteBuf, tag);
-		byteBuf.setIndex(0, byteBuf.capacity());
-		var netTag = SNBTNet.readCompound(byteBuf);
-
-		System.out.println("Network IO test: " + (tag.equals(netTag) ? "success" : "fail"));
+		return tag;
 	}
 }
