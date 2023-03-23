6bc468228752143303d5e67ff9e88e3364dc258a
Des Herriott <des.herriott@gmail.com>
2023-03-23 09:30:17 +0000

    feat: add proof of concept display name fabric event
    Equivalent to Forge's PlayerEvent.NameFormat, used by FTB Ranks
    and FTB Essentials to modify player's display name based on rank,
    nickname & recording/streaming status.  Much cleaner than both mods
    using their own mixins, and easier to establish event order.
    
    If this event ends up in Fabric API in some form, this will go away.

---
+++ fabric/src/main/java/dev/ftb/mods/ftblibrary/fabric/PlayerDisplayNameCallback.java
@@ -0,0 +1,23 @@
+package dev.ftb.mods.ftblibrary.fabric;
+
+import dev.ftb.mods.ftblibrary.FTBLibrary;
+import net.fabricmc.fabric.api.event.Event;
+import net.fabricmc.fabric.api.event.EventFactory;
+import net.minecraft.network.chat.Component;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.world.entity.player.Player;
+
+public interface PlayerDisplayNameCallback {
+    ResourceLocation EARLY = new ResourceLocation(FTBLibrary.MOD_ID, "early");
+    ResourceLocation LATE = new ResourceLocation(FTBLibrary.MOD_ID, "late");
+
+    Event<PlayerDisplayNameCallback> EVENT = EventFactory.createWithPhases(PlayerDisplayNameCallback.class,
+            (listeners) -> ((player, oldDisplayName) -> {
+                for (PlayerDisplayNameCallback event : listeners) {
+                    oldDisplayName = event.modifyDisplayName(player, oldDisplayName);
+                }
+                return oldDisplayName;
+            }), EARLY, Event.DEFAULT_PHASE, LATE);
+
+    Component modifyDisplayName(Player player, Component oldDisplayName);
+}
