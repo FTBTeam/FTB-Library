8b611061c7ab5c8059f005233f4b9ddd9c612052
Des Herriott <des.herriott@gmail.com>
2023-12-12 15:34:46 +0000

    build: version -> 2004.1.1

--- neoforge/src/main/java/dev/ftb/mods/ftblibrary/neoforge/FTBLibraryNeoForge.java
+++ neoforge/src/main/java/dev/ftb/mods/ftblibrary/neoforge/FTBLibraryNeoForge.java
@@ -6,7 +6,6 @@ import net.neoforged.fml.common.Mod;
 @Mod(FTBLibrary.MOD_ID)
 public class FTBLibraryNeoForge {
 	public FTBLibraryNeoForge() {
-		// EventBuses.registerModEventBus(FTBLibrary.MOD_ID, FMLJavaModLoadingContext.get().getModEventBus());
 		new FTBLibrary();
 	}
 }
