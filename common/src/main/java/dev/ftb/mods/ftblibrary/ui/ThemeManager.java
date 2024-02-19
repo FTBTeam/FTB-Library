bf15df95b97becfc01dff45498d2e40b8757981c
Des Herriott <des.herriott@gmail.com>
2024-02-19 16:59:53 +0000

    chore: ThemeManager class got missed

---
+++ common/src/main/java/dev/ftb/mods/ftblibrary/ui/ThemeManager.java
@@ -0,0 +1,12 @@
+package dev.ftb.mods.ftblibrary.ui;
+
+/**
+ * This is just a stub for now. Eventually there will be a way for players to select a UI theme!
+ */
+public enum ThemeManager {
+    INSTANCE;
+
+    public Theme getActiveTheme() {
+        return NordTheme.THEME;
+    }
+}
