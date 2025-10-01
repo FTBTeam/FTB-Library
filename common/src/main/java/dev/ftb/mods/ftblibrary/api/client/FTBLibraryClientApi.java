32d035264fdfa316da1677c46b2f50c425756088
Michael Hillcox <michaelhillcox@gmail.com>
2025-10-01 17:33:50 +0100

    feat: client API class and sidebar screen blacklist API

---
+++ common/src/main/java/dev/ftb/mods/ftblibrary/api/client/FTBLibraryClientApi.java
@@ -0,0 +1,40 @@
+package dev.ftb.mods.ftblibrary.api.client;
+
+import com.google.common.collect.ImmutableSet;
+import net.minecraft.client.gui.screens.Screen;
+import org.jetbrains.annotations.Nullable;
+
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Set;
+
+public class FTBLibraryClientApi {
+    private static FTBLibraryClientApi INSTANCE;
+
+    public static FTBLibraryClientApi get() {
+        if (INSTANCE == null) {
+            INSTANCE = new FTBLibraryClientApi();
+        }
+
+        return INSTANCE;
+    }
+
+
+    public Set<String> screenSidebarBlacklist = new HashSet<>();
+
+    private FTBLibraryClientApi() {
+    }
+
+    public void addSidebarScreenBlacklist(String ...screenClass) {
+        Collections.addAll(screenSidebarBlacklist, screenClass);
+    }
+
+    public boolean isSidebarScreenBlacklisted(@Nullable Screen screen) {
+        System.out.printf("Checking sidebar blacklist for screen: %s%n", screen == null ? "null" : screen.getClass().getName());
+        return screen != null && screenSidebarBlacklist.contains(screen.getClass().getName());
+    }
+
+    public Set<String> getSidebarBlacklist() {
+        return ImmutableSet.copyOf(screenSidebarBlacklist);
+    }
+}
