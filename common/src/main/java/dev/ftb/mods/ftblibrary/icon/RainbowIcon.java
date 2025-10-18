b23ab44db15c13f990f6fb6da759df1dea7debe4
Des Herriott <des.herriott@gmail.com>
2025-10-18 09:26:02 +0100

    chore: post cherry-pick fixup

--- common/src/main/java/dev/ftb/mods/ftblibrary/icon/RainbowIcon.java
+++ common/src/main/java/dev/ftb/mods/ftblibrary/icon/RainbowIcon.java
@@ -1,15 +1,8 @@
 package dev.ftb.mods.ftblibrary.icon;
 
-import com.mojang.blaze3d.systems.RenderSystem;
-import com.mojang.blaze3d.vertex.BufferUploader;
-import com.mojang.blaze3d.vertex.DefaultVertexFormat;
-import com.mojang.blaze3d.vertex.Tesselator;
-import com.mojang.blaze3d.vertex.VertexFormat;
-import dev.ftb.mods.ftblibrary.ui.GuiHelper;
 import dev.ftb.mods.ftblibrary.util.text.RainbowTextColor;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.gui.GuiGraphics;
-import net.minecraft.client.renderer.GameRenderer;
 
 public class RainbowIcon extends Icon {
     public static RainbowIcon RAINBOW = new RainbowIcon();
@@ -25,11 +18,6 @@ public class RainbowIcon extends Icon {
         int ticks = (int) Minecraft.getInstance().clientTickCount;
         int color =  colors[ticks % colors.length];
 
-        RenderSystem.setShader(GameRenderer::getPositionColorShader);
-        RenderSystem.setShaderColor(1F, 1F, 1F, 1F);
-
-        var buffer = Tesselator.getInstance().begin(VertexFormat.Mode.QUADS, DefaultVertexFormat.POSITION_COLOR);
-        GuiHelper.addRectToBuffer(graphics, buffer, x, y, width, height, Color4I.rgb(color));
-        BufferUploader.drawWithShader(buffer.buildOrThrow());
+        graphics.fill(x, y, x + width, y + height, color);
     }
 }
