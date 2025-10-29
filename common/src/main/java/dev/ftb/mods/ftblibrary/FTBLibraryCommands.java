1ed008cd04bdf6de6d6121750a079beb38defa7c
Des Herriott <des.herriott@gmail.com>
2025-10-29 12:52:20 +0000

    fix: make InfoBuilder.create() public

--- common/src/main/java/dev/ftb/mods/ftblibrary/FTBLibraryCommands.java
+++ common/src/main/java/dev/ftb/mods/ftblibrary/FTBLibraryCommands.java
@@ -251,7 +251,7 @@ public class FTBLibraryCommands {
     }
 
     public record InfoBuilder(ListTag list, HolderLookup.Provider provider) {
-        static InfoBuilder create(CommandContext<CommandSourceStack> context) {
+        public static InfoBuilder create(CommandContext<CommandSourceStack> context) {
             return new InfoBuilder(new ListTag(), context.getSource().registryAccess());
         }
 
