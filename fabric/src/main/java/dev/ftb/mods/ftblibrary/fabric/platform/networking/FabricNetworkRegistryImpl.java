6d7e494ae787cf255cb095e75e37e880791601f1
Michael Hillcox <michaelhillcox@gmail.com>
2026-03-13 16:06:40 +0000

    fix: packet handling

--- fabric/src/main/java/dev/ftb/mods/ftblibrary/fabric/platform/networking/FabricNetworkRegistryImpl.java
+++ fabric/src/main/java/dev/ftb/mods/ftblibrary/fabric/platform/networking/FabricNetworkRegistryImpl.java
@@ -1,6 +1,5 @@
 package dev.ftb.mods.ftblibrary.fabric.platform.networking;
 
-import dev.ftb.mods.ftblibrary.platform.Platform;
 import dev.ftb.mods.ftblibrary.platform.network.NetworkRegistry;
 import net.fabricmc.fabric.api.client.networking.v1.ClientPlayNetworking;
 import net.fabricmc.fabric.api.networking.v1.PayloadTypeRegistry;
@@ -15,18 +14,16 @@ public class FabricNetworkRegistryImpl implements NetworkRegistry {
         PayloadTypeRegistry.clientboundPlay().register(type, codec);
 
         // Only register the receiver on the client (I think)
-        if (!Platform.get().env().isClient()) {
-            ClientPlayNetworking.registerGlobalReceiver(type, (payload, context) -> {
-                Server2ClientContext createdContext = new Server2ClientContext(context);
-                handler.handle(payload, createdContext);
+        ClientPlayNetworking.registerGlobalReceiver(type, (payload, context) -> {
+            Server2ClientContext createdContext = new Server2ClientContext(context);
+            handler.handle(payload, createdContext);
 
-                // TODO: We might not need this.
-                if (createdContext.task() != null) {
-                    // Run anything that has been enqueued
-                    createdContext.task().run();
-                }
-            });
-        }
+            // TODO: We might not need this.
+            if (createdContext.task() != null) {
+                // Run anything that has been enqueued
+                createdContext.task().run();
+            }
+        });
     }
 
     @Override
