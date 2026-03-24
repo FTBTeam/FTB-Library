eae5a9cd7ee39b96151baa3ed1189ee3c37eee61
Des Herriott <des.herriott@gmail.com>
2026-03-24 17:03:33 +0000

    chore: couple more minor changes

--- common/src/main/java/dev/ftb/mods/ftblibrary/platform/event/NativeEventPosting.java
+++ common/src/main/java/dev/ftb/mods/ftblibrary/platform/event/NativeEventPosting.java
@@ -5,6 +5,8 @@ import java.util.concurrent.ConcurrentHashMap;
 import java.util.function.Consumer;
 import java.util.function.Function;
 
+/// Allows platform-independent posting of our own mods' "events", by mapping an event data object (typically a
+/// record) to a `Consumer` or `Function` which handles posting the corresponding native NeoForge/Fabric event.
 public enum NativeEventPosting {
     INSTANCE;
 
@@ -15,12 +17,26 @@ public enum NativeEventPosting {
         return INSTANCE;
     }
 
+    /// Register a `Consumer` event poster; this must be called in platform-dependent code, since the consumer handles
+    /// posting of the native platform event. `Consumer` posters are used for events which do not return any data.
+    ///
+    /// @param cls the class of the data structure which will later be posted via [#postEvent(Object)]
+    /// @param dataHandler the consumer object
+    /// @param <T> type of the data to be posted
     public <T> void registerEvent(Class<T> cls, Consumer<T> dataHandler) {
         consumers.put(cls, dataHandler);
     }
 
-    public <T, R> void registerEventWithResult(TypedEvent<T, R> event, Function<T, R> mapper) {
-        functions.put(event, mapper);
+    /// Register a `Function` event poster; this must be called in platform-dependent code, since the consumer handles
+    /// posting of the native platform event. `Function` posters are used for events which return a result of some kind;
+    /// the type of the result is defined in the [TypedEvent] parameter.
+    ///
+    /// @param eventType the class and event return type of the data structure which will later
+    /// @param mapper the function which takes the typed event and posts a corresponding native event,
+    /// and returns the event result
+    /// @param <T> type of the data to be posted
+    public <T, R> void registerEventWithResult(TypedEvent<T, R> eventType, Function<T, R> mapper) {
+        functions.put(eventType, mapper);
     }
 
     public <T> void postEvent(T data) {
