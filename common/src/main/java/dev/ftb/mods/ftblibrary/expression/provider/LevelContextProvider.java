8818dfe43b00697cd91fdcb81336d7f2cb4cd3c5
Michael Hillcox <michaelhillcox@gmail.com>
2026-04-20 13:49:33 +0100

    feat: expanded default contexts

--- common/src/main/java/dev/ftb/mods/ftblibrary/expression/provider/LevelContextProvider.java
+++ common/src/main/java/dev/ftb/mods/ftblibrary/expression/provider/LevelContextProvider.java
@@ -1,5 +1,6 @@
 package dev.ftb.mods.ftblibrary.expression.provider;
 
+import net.minecraft.world.Difficulty;
 import net.minecraft.world.level.Level;
 
 /// Context provider for a [Level] providing support for {@code level.} expressions.
@@ -11,15 +12,68 @@ public class LevelContextProvider extends ContextProvider {
         this.level = level;
     }
 
-    public boolean isDay() {
-        return this.level.getGameTime() % 24000L < 12000L;
+    public long getClockTime() {
+        return this.level.getDefaultClockTime();
     }
 
-    public boolean isNight() {
-        return !isDay();
+    public long getGameTime() {
+        return this.level.getGameTime();
+    }
+
+    public boolean isPeaceful() {
+        return this.level.getDifficulty() == Difficulty.PEACEFUL;
+    }
+
+    public boolean isEasy() {
+        return this.level.getDifficulty() == Difficulty.EASY;
+    }
+
+    public boolean isNormal() {
+        return this.level.getDifficulty() == Difficulty.NORMAL;
+    }
+
+    public boolean isHard() {
+        return this.level.getDifficulty() == Difficulty.HARD;
+    }
+
+    public boolean isOverworld() {
+        return this.level.dimension() == Level.OVERWORLD;
+    }
+
+    public boolean isNether() {
+        return this.level.dimension() == Level.NETHER;
+    }
+
+    public boolean isEnd() {
+        return this.level.dimension() == Level.END;
+    }
+
+    /// Checks if the time is within the day range.
+    public boolean isBrightOutside() {
+        return this.level.isBrightOutside();
+    }
+
+    public boolean isDarkOutside() {
+        return !this.level.isBrightOutside();
     }
 
     public boolean isRaining() {
         return this.level.isRaining();
+    }
+
+    public boolean isThundering() {
+        return this.level.isThundering();
+    }
+
+    public int randomInt() {
+        return this.level.getRandom().nextInt();
+    }
+
+    public float randomFloat() {
+        return this.level.getRandom().nextFloat();
+    }
+
+    public double randomDouble() {
+        return this.level.getRandom().nextDouble();
     }
 }
--- common/src/main/java/dev/ftb/mods/ftblibrary/expression/provider/PlayerContextProvider.java
+++ common/src/main/java/dev/ftb/mods/ftblibrary/expression/provider/PlayerContextProvider.java
@@ -1,7 +1,15 @@
 package dev.ftb.mods.ftblibrary.expression.provider;
 
+import dev.ftb.mods.ftblibrary.platform.Platform;
+import net.minecraft.core.BlockPos;
+import net.minecraft.server.MinecraftServer;
+import net.minecraft.server.dedicated.DedicatedServer;
+import net.minecraft.server.level.ServerLevel;
 import net.minecraft.server.permissions.Permissions;
+import net.minecraft.util.Mth;
 import net.minecraft.world.entity.player.Player;
+import net.minecraft.world.level.GameType;
+import net.minecraft.world.level.Level;
 
 /// Context provider for a [Player] providing support for {@code player.} expressions.
 public class PlayerContextProvider extends ContextProvider {
@@ -12,19 +20,66 @@ public class PlayerContextProvider extends ContextProvider {
         this.player = player;
     }
 
+    /// Checks if the player is an OP
     public boolean isOp() {
         return this.player.permissions().hasPermission(Permissions.COMMANDS_GAMEMASTER);
     }
 
-    public boolean hasStage(String stage) {
-        return false;
+    public boolean isCreative() {
+        return this.player.isCreative();
     }
 
+    public boolean isSpectator() {
+        return this.player.isSpectator();
+    }
+
+    public boolean isSurvival() {
+        return this.player.gameMode() == GameType.SURVIVAL;
+    }
+
+    public boolean isAdventure() {
+        return this.player.gameMode() == GameType.ADVENTURE;
+    }
+
+    public boolean isSleeping() {
+        return this.player.isSleeping();
+    }
+
+    public boolean isSwimming() {
+        return this.player.isSwimming();
+    }
+
+    public boolean inContainer() {
+        return this.player.containerMenu != this.player.inventoryMenu;
+    }
+
+    /// Checks if the player has the given tag
     public boolean hasTag(String tag) {
         return player.entityTags().contains(tag);
     }
 
+    /// Checks if the player is an implementation of a Fake Player or not.
+    public boolean isFake() {
+        return Platform.get().misc().isFakePlayer(player);
+    }
+
+    public boolean atSpawn() {
+        if (player.level() instanceof ServerLevel serverLevel && serverLevel.dimension() == Level.OVERWORLD && getSpawnProtectionRadius(serverLevel.getServer()) > 0) {
+            BlockPos spawn = serverLevel.getRespawnData().pos();
+            int x = Mth.abs(Mth.floor(player.getX()) - spawn.getX());
+            int z = Mth.abs(Mth.floor(player.getZ()) - spawn.getZ());
+            return Math.max(x, z) <= getSpawnProtectionRadius(serverLevel.getServer());
+        }
+
+        return false;
+    }
+
     public String playerName() {
         return player.getName().getString();
+    }
+
+    /// Helper to figure out the spawn protection radius, which is only available on a dedicated server instance
+    private static int getSpawnProtectionRadius(MinecraftServer server) {
+        return server instanceof DedicatedServer d ? d.spawnProtectionRadius() : 0;
     }
 }
